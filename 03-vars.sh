2225d455454546c15fde5810103aa26472a532e8
Swapnil <sark710@gmail.com>
2022-02-04 00:09:08 -0500

    Variable

--- 03-vars.sh
+++ 03-vars.sh
@@ -8,3 +8,8 @@ echo student_name = $student_name
 DATE=2022-02-03
 
 echo Good Morning $student_name, Today Date is $DATE
+
+## Command substitution
+DATE=$(date +%F)
+
+echo Good Morning $student_name, Today Date is $DATE
