c0052144a4cd684f31e83386946da9d744e26b48
Swapnil <sark710@gmail.com>
2022-02-03 23:39:52 -0500

    Print colors

--- 02-print.sh
+++ 02-print.sh
@@ -21,3 +21,5 @@ echo Hello World
 
 
 echo -e "\e[31mRED\e[32mGreen\e[33mYellow\e[34mBlue\e[35mMagenta\e[36mCyan\e[0m"
+
+echo -e "\e[1;31mRED\e[1;32mGreen\e[1;33mYellow\e[1;34mBlue\e[1;35mMagenta\e[1;36mCyan\e[0m"
