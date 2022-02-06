fd44641b32e129ac67e8eab00d5f25858efc4aec
Swapnil <sark710@gmail.com>
2022-02-05 23:42:11 -0500

    Updating roboshop-project

--- roboshop-project/components/frontend.sh
+++ roboshop-project/components/frontend.sh
@@ -38,3 +38,6 @@
 
 echo "Installing NGINX"
 yum install nginx -y >/tmp/roboshop.log
+
+echo "Download the Frontend content"
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" > /tmp/roboshop.log
