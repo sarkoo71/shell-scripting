cbc3461f47f3ee1c2ff609f9de64cd99d134cb13
Swapnil <sark710@gmail.com>
2022-02-20 02:33:20 -0500

    Updating roboshop-project

--- roboshop-project/create-ec2.sh
+++ roboshop-project/create-ec2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-AMI_ID = $(aws ec2 describe-images  --filters "Name=name,Values=Centos-7-DevOps-Practice" --query 'Images[*].[ImageId]' --output text)
+AMI_ID=$(aws ec2 describe-images  --filters "Name=name,Values=Centos-7-DevOps-Practice" --query 'Images[*].[ImageId]' --output text)
 
 if[ -z "${AMI_ID}"]; then
   echo "unable to find image AMID"
