b147ecc2c63be6277ca06b89fd94e222ad125929
Swapnil Jariwala <sark710@gmail.com>
2022-03-13 20:51:25 -0500

    del

--- roboshop-project/create-ec2.sh
+++ roboshop-project/create-ec2.sh
@@ -2,10 +2,10 @@
 
 AMI_ID=$(aws ec2 describe-images  --filters "Name=name,Values=Centos-7-DevOps-Practice")
 echo AMI_ID
-#if [ -z "${AMI_ID}" ];then
-#  echo -e "\e[1;31munable to find image AMID\e[0m"
-#else
-#  echo -e "\e[1;32mAMI_ID = ${AMI_ID}\e[0m"
-#fi
-#
-#aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro --output text
+if [ -z "${AMI_ID}" ];then
+  echo -e "\e[1;31munable to find image AMID\e[0m"
+else
+  echo -e "\e[1;32mAMI_ID = ${AMI_ID}\e[0m"
+fi
+
+aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro --output text
