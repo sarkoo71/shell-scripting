6090ca85b7e53bb0bf5561bb9c09e7e33438041c
Swapnil Jariwala <sark710@gmail.com>
2022-03-16 00:15:09 -0500

    Updating roboshop-project

--- roboshop-project/create-ec2.sh
+++ roboshop-project/create-ec2.sh
@@ -16,4 +16,12 @@ else
   echo -e "\e[1;32mAMI_ID = ${AMI_ID}\e[0m"
 fi
 
-aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro --output text --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${Instance_Name}}]"
+
+Private_Ip=$(aws ec2 describe-instances --filters Name=tag:Name,Values=frontend --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text)
+
+if [-z '${Private_Ip}']; then
+  aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro --output text --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${Instance_Name}}]"
+else
+  echo "Instance ${Instance_Name} is already exists, hence not creating"
+  exit
+fi
