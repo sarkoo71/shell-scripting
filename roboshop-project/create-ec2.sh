69ed0603fecf7e75dd2eb166649a3d960f1ec80f
Swapnil Jariwala <sark710@gmail.com>
2022-03-15 23:45:13 -0500

    Updating roboshop-project

--- roboshop-project/create-ec2.sh
+++ roboshop-project/create-ec2.sh
@@ -11,8 +11,9 @@ AMI_ID=$(aws ec2 describe-images  --filters "Name=name,Values=Centos-7-DevOps-Pr
 
 if [ -z "${AMI_ID}" ];then
   echo -e "\e[1;31munable to find image AMID\e[0m"
+  exit
 else
   echo -e "\e[1;32mAMI_ID = ${AMI_ID}\e[0m"
 fi
 
-aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro --output text
+aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro --output text --tag-specifications "ResourceType=instance,Tags=[{Key=name,Value=${Instance_Name}}]"
