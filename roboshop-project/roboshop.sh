517671a94a78e0562cf3b9daf3169a78ca885189
swapnil <sark710@gmail.com>
2022-07-26 01:49:15 -0500

    updating .

--- roboshop-project/roboshop.sh
+++ roboshop-project/roboshop.sh
@@ -3,7 +3,7 @@
 ID=$(id -u)
 if [ $ID -ne 0 ]; then
   echo -e "\e[1;31m You should be root user \e[0m"
-  exit
+  exit 1
 fi
 
 if [ -f components/$1.sh ]
@@ -11,4 +11,6 @@ then
   bash components/$1.sh
 else
     echo -e "\e[1;31mWrong input\e[0m"
+    echo -e "\e[1;32m Available inputs are - frontend|mongodb|catalogue|cart|dispatch|mysql|payment|rabbimq|redis|shipping|user\e[0m"
+    exit 1
 fi
