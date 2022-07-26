d185dba84bd4cc872ca6287b95c72c6e7b65293a
swapnil <sark710@gmail.com>
2022-07-26 00:18:14 -0500

    updating .

--- roboshop-project/create-ec2.sh
+++ roboshop-project/create-ec2.sh
@@ -50,3 +50,4 @@ echo '{
 
 ZONE_ID=$(aws route53 list-hosted-zones --query "HostedZones[*].{name:Name,ID:Id}" --output text | grep roboshop.internal  | awk '{print $1}' | awk -F / '{print $3}')
 aws route53 change-resource-record-sets --hosted-zone-id $ZONE_ID --change-batch file:///tmp/record.json --output text &>>$LOG
+echo -e "\e[1m DNS record created \e[0m"
