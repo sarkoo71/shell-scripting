06e779e767e2074ef771f75ef31fd6d5b33dd1c3
swapnil <sark710@gmail.com>
2022-07-26 01:28:32 -0500

    STAT function

--- roboshop-project/components/frontend.sh
+++ roboshop-project/components/frontend.sh
@@ -6,20 +6,26 @@ STAT $?
 
 echo "Download the Frontend content"
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>$LOG_FILE
+STAT $?
 
 echo "Clean Old Content"
 rm -rf /usr/share/nginx/html/* &>>$LOG_FILE
+STAT $?
 
 echo "Extracted Frontend Content"
 cd /tmp
 unzip -o frontend.zip &>>$LOG_FILE
+STAT $?
 
 echo "Copy Extracted Content to Nginx Path"
 cp -r frontend-main/static/* /usr/share/nginx/html/ &>>$LOG_FILE
+STAT $?
 
 echo "Copy NGINX roboshop config"
 cp frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG_FILE
+STAT $?
 
 echo "Start NGINX Service"
 systemctl enable nginx &>>$LOG_FILE
 systemctl start nginx &>>$LOG_FILE
+STAT $?
