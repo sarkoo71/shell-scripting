0843f123bb75c8c29996b8f991630b6a1a20485c
swapnil <sark710@gmail.com>
2022-08-16 00:12:54 -0500

    updating .

--- roboshop-project/components/redis.sh
+++ roboshop-project/components/redis.sh
@@ -20,22 +20,22 @@ source components/common.sh
 #```
 
 echo "Configuring redis repo"
-curl -L https://raw.githubusercontent.com/roboshop-devops-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo &>>$LOG_FILE_
+curl -L https://raw.githubusercontent.com/roboshop-devops-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo &>>$LOG_FILE
 STAT $?
 
 echo "Install Redis"
-yum install redis-6.2.7 -y &>>LOG_FILE_
+yum install redis-6.2.7 -y &>>LOG_FILE
 STAT $?
 
 echo "Update redis configuration"
 if [ -f  /etc/redis.conf ]; then
-    sed -i -e "s/127.0.0.1/0.0.0.0/g" /etc/redis.conf &>>$LOG_FILE_
+    sed -i -e "s/127.0.0.1/0.0.0.0/g" /etc/redis.conf &>>$LOG_FILE
 elif [ -f /etc/redis/redis.conf ]; then
-    sed -i -e "s/127.0.0.1/0.0.0.0/g" /etc/redis/redis.conf &>>$LOG_FILE_
+    sed -i -e "s/127.0.0.1/0.0.0.0/g" /etc/redis/redis.conf &>>$LOG_FILE
 fi
 STAT $?
 
 echo "Start Redis"
-systemctl enable redis &>>$LOG_FILE_
-systemctl start redis &>>$LOG_FILE_
+systemctl enable redis &>>$LOG_FILE
+systemctl start redis &>>$LOG_FILE
 STAT $?
