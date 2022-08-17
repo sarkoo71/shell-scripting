3b926c6ccf08e25b6793c6f1e0a23706b6a9d736
swapnil <sark710@gmail.com>
2022-08-16 23:34:26 -0500

    updating .

--- roboshop-project/components/common.sh
+++ roboshop-project/components/common.sh
@@ -15,11 +15,11 @@ STAT(){
 NODEJS(){
   COMPONENT=$1
   echo "Setup NodeJS Repo"
-  curl -sL https://rpm.nodesource.com/setup_10.x | sudo bash - &>>$LOG_FILE
+  curl -fsSL https://rpm.nodesource.com/setup_lts.x | bash - &>>$LOG_FILE
   STAT $?
 
   echo "Install NodeJS"
-  yum install nodejs -y &>>$LOG_FILE
+  yum install nodejs gcc-c++ -y &>>$LOG_FILE
   STAT $?
 
   echo "Create app user"
