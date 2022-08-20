01812ac675c66b7a8653f6aee2b5659ad1b89d45
swapnil <sark710@gmail.com>
2022-08-20 04:01:00 -0500

    updating .

--- roboshop-project/components/common.sh
+++ roboshop-project/components/common.sh
@@ -22,8 +22,8 @@ NODEJS(){
   echo "Install NodeJS by NVM"
   export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
   [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-  nvm install 16
   yum install npm
+  nvm install 16
   STAT $?
 
   echo "Create app user"
